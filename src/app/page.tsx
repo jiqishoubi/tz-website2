5ab8091993e87fdf1e9ccc460e5e897ad38626fa
jiqishoubi <lntz2014@gmail.com>
2025-03-18 17:33:04 +0800

    chore: 测试cicd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            5 Get started by editing <code>src/app/page.tsx</code>.
+            6 Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
