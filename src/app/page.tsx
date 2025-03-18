7b2f2f87c299f26a7bca874ecde770dae973e840
jiqishoubi <lntz2014@gmail.com>
2025-03-18 17:30:03 +0800

    pm2 启动命令

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            4 Get started by editing <code>src/app/page.tsx</code>.
+            5 Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
