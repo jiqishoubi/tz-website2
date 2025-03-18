48decc8f940ada727b0524f333161b796951db54
jiqishoubi <lntz2014@gmail.com>
2025-03-18 17:06:03 +0800

    npm install --omit=dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            2 Get started by editing <code>src/app/page.tsx</code>.
+            3 Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
