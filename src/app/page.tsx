20458e0b88eff33842e239e5c78849c19c2d2f82
jiqishoubi <lntz2014@gmail.com>
2025-03-19 14:32:48 +0800

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            7 Get started by editing <code>src/app/page.tsx</code>.
+            8 Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
