c91bfe9f83d5636a7825a49ca453c09be29ddb4f
jiqishoubi <lntz2014@gmail.com>
2025-03-20 14:12:34 +0800

    基础页面

---
+++ src/app/archive/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Index: React.FC = () => {
+  return <>archive</>
+}
+
+export default Index
---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { primaryColor } from '@/utils/consts'
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+import React from 'react'
+
+const linkList = [
+  { href: '/', label: 'Home' },
+  { href: '/archive', label: 'Archive' },
+  { href: '/stack', label: 'Stack' },
+  { href: '/about', label: 'About' },
+]
+
+function Logo() {
+  return (
+    <div className="inline-flex justify-center items-center font-bold text-[17px] cursor-pointer">
+      <span>Tong</span>
+      <span className="inline-flex items-center justify-center w-[44px] h-[44px] text-white rounded-[7px] ml-[7px]" style={{ backgroundColor: primaryColor }}>
+        Zhou
+      </span>
+    </div>
+  )
+}
+
+const Index: React.FC = () => {
+  const pathname = usePathname()
+  // console.log(`🚀 ~ pathname`, pathname)
+  return (
+    <nav className="flex justify-between items-center p-4">
+      <div className="w-[100px]">
+        <Logo />
+      </div>
+      <div className="flex-1 flex justify-center">
+        <div className="md:block flex">
+          {linkList.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className="text-[17px] mx-[17px]"
+              style={pathname === link.href ? { color: primaryColor, textDecoration: 'underline', textUnderlineOffset: '6px' } : {}}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="w-[100px]"></div>
+      <div className="md:hidden">啦啦</div>
+    </nav>
+  )
+}
+
+export default Index
