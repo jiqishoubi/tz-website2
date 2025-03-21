56a5bf0c844ca258b8de55d60d061c99ef086d7a
jiqishoubi <lntz2014@gmail.com>
2025-03-21 16:48:24 +0800

    fix

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -8,7 +8,11 @@ export default function RootLayout({
 }>) {
   return (
     // 比minHeight长一点 主要让滚动条显示出来 防止布局抖动
-    <main className="max-w-[1300px] mx-auto min-h-screen min-w-[500px] flex flex-col" style={{ minHeight: 'calc(100vh + 30px)' }}>
+    // 600px 和 100vh 选一个最大的
+    <main
+      className="max-w-[1300px] mx-auto min-h-[max(600px,calc(100vh+30px))] flex flex-col"
+      // style={{ minHeight: 'calc(100vh + 30px)' }}
+    >
       <Nav />
       <div className="flex-1">{children}</div>
       <Footer />
